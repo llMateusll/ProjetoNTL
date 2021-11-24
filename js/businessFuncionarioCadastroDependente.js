5dd4d30189789d7ceef5da9b9d2b457cc069b4ff
MateusSilva <mateus.d.santana.99@gmail.com>
2021-11-24 11:02:23 -0300

    dependente

---
+++ js/businessFuncionarioCadastroDependente.js
@@ -0,0 +1,49 @@
+function validaSexoExistente(sexo, callback) {
+    $.ajax({
+        url: 'js/sqlscope_funcionarioCadastroSexo.php', //caminho do arquivo a ser executado
+        dataType: 'html', //tipo do retorno
+        type: 'post', //metodo de envio
+        data: { funcao: 'validaSexo', sexo: sexo }, //valores enviados ao script      
+        success: function (data) {
+
+            callback(data);
+        }
+    });
+}
+
+function gravarSexo(id,ativo,sexo, callback) {
+    $.ajax({
+        url: 'js/sqlscope_funcionarioCadastroSexo.php',
+        dataType: 'html', //tipo do retorno
+        type: 'post', //metodo de envio
+        data: {funcao: "gravarSexo",id: id, ativo: ativo, sexo: sexo},   
+        success: function (data) {
+        callback(data);
+        } 
+    }); 
+}
+function recuperaFuncionario(id, callback) {
+    $.ajax({
+        url: 'js/sqlscope_funcionarioCadastroSexo.php', //caminho do arquivo a ser executado
+        dataType: 'html', //tipo do retorno
+        type: 'post', //metodo de envio
+        data: {funcao: 'recupera', id: id}, //valores enviados ao script      
+        success: function (data) {
+            callback(data); 
+        }
+    });
+
+    return;
+}
+function excluirGrupo(id, callback) {
+    $.ajax({
+        url: 'js/sqlscope_funcionarioCadastroSexo.php', //caminho do arquivo a ser executado
+        dataType: 'html', //tipo do retorno
+        type: 'post', //metodo de envio
+        data: {funcao: 'excluir', id: id}, //valores enviados ao script      
+        success: function (data) {
+            callback(data); 
+        }
+    });
+}
+   
