3224167c2bb2b431d6bceff4b2204555fa309d47
MateusSilva <mateus.d.santana.99@gmail.com>
2021-12-20 15:19:05 -0300

    Correção

--- funcionarioCadastro.php
+++ funcionarioCadastro.php
@@ -96,8 +96,8 @@ include("inc/nav.php");
                                                                 <label class="label">Ativo</label>
                                                                 <label class="select">
                                                                     <select id="ativo" name="ativo" class="required">
-                                                                        <option value="0">Não</option>
                                                                         <option value="1">Sim</option>
+                                                                        <option value="0">Não</option>
                                                                     </select><i></i>
                                                                 </label>
                                                             </section>
